d7f77e657fe7a46370757b40a33188619599c82a
leejeongpil <ijpsm9@gmail.com>
2021-08-01 17:20:24 +0900

    post test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "[Jekyll] Test",
+        "excerpt":"what the?  ","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/blog/first-post-test/",
+        "teaser": null
+      }]
